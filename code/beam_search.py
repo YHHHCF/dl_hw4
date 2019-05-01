3e5b2b8391d56453bb7327de127419bc1f893755
YHHHCF <huangzhejinthu@hotmail.com>
2019-05-01 13:55:00 -0400

    bs

--- code/beam_search.py
+++ code/beam_search.py
@@ -1,7 +1,41 @@
+import numpy as np
+from queue import PriorityQueue as PQ
 
 
-# input is the input of the function(model)
-# width is the beam width, func is the function to generate P(y[t]|y[0:t])
-def beam_search(input, func, width):
-    best_seq = None
-    return best_seq
+# get prob from input char
+def model(char):
+    prob = np.random.rand(34)
+    prob = prob / sum(prob)
+    return prob
+
+
+# append the input char to the list
+def append_char(char_list, char):
+    if char_list:
+        char_list = np.append(char_list, char)
+    else:
+        char_list = [char]
+    return char_list
+
+
+def expand_searcher(searcher):
+    return searcher
+
+
+def clear_searcher(searcher):
+    return searcher
+
+
+if __name__ == '__main__':
+    beam_width = 32
+    searcher = PQ()
+    pool = PQ()
+
+    char = 32
+    end_symb = 33
+
+    while pool.qsize() < beam_width:
+        searcher.put((1, append_char(None, char)))
+        prob = model(char)
+        searcher = expand_searcher(searcher)
+        searcher = clear_searcher(searcher)
